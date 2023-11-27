d904b0abf95a1ff21f130b91ece59428961b3e67
Krishna Gupta <121439479+krishnagptamcs@users.noreply.github.com>
2023-11-27 10:17:10 +0530

    typescript added

---
+++ index.ts
@@ -0,0 +1,11 @@
+// Typescript is nothing but , it is a super set of javascript with extra power , intoduced by Microsoft in 2012, it help to define our varaible data type , so that no bug and faster compilation can be done , and getting a bug free at time of production build.
+
+console.log("Krishna");
+
+var num = 5;
+
+num = 10;
+
+console.log(num);
+
+ 
---
+++ index2.ts
@@ -0,0 +1,8 @@
+const  sum= (a:number,b:number):number=>{
+    return a+b;
+};
+
+console.log(sum(10,35));
+
+
+
